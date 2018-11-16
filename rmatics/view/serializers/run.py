f6aec831c45e2164d5249a7c58350a2ab7c83a88
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2018-11-16 16:53:37 +0300

    refactoring: Rename run schemas

--- rmatics/view/serializers/run.py
+++ rmatics/view/serializers/run.py
@@ -1,20 +1,20 @@
 from marshmallow import Schema, fields
 
 
-class UserSchema(Schema):
+class UserRunSchema(Schema):
     id = fields.Integer(dumps_only=True)
     firstname = fields.String()
     lastname = fields.String()
 
 
-class ProblemSchema(Schema):
+class ProblemRunSchema(Schema):
     id = fields.Integer(dumps_only=True)
     name = fields.String()
 
 
 class RunSchema(Schema):
     id = fields.Integer(dumps_only=True)
-    user = fields.Nested(UserSchema)
-    problem = fields.Nested(ProblemSchema)
+    user = fields.Nested(UserRunSchema)
+    problem = fields.Nested(ProblemRunSchema)
     ejudge_status = fields.Integer()
     create_time = fields.DateTime()
