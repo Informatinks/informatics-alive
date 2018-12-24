6cb133e8514a7aeb661040300ca9e214c4466fdc
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2018-12-24 22:21:58 +0300

    feat: Add cli for testsing FALSK_APP=rmatics.wsgi:application flask test

--- rmatics/__init__.py
+++ rmatics/__init__.py
@@ -1,7 +1,26 @@
-from flask import Flask
 from logging.config import dictConfig
 
+from flask import Flask
+from werkzeug.exceptions import HTTPException
+
+from rmatics.model.base import db
+from rmatics.model.base import mongo
+from rmatics.model.base import redis
+from rmatics.view import handle_api_exception, load_user
+from rmatics.view.bootstrap import bootstrap
 from rmatics.view.centrifugo import centrifugo_client
+from rmatics.view.course import course_blueprint
+from rmatics.view.course.statement import statement_blueprint
+from rmatics.view.problem.route import problem_blueprint
+from rmatics.view.problem.submit import submit
+from rmatics.view.protocol import protocol
+from rmatics.view.user.auth import auth
+from rmatics.view.user.group import group
+from rmatics.view.user.group_invite import group_invite
+from rmatics.view.user.notification import notification
+from rmatics.view.user.user import user
+from rmatics.utils import url_encoder
+from rmatics import cli
 
 
 def create_app(config=None):
@@ -30,16 +49,8 @@ def create_app(config=None):
         app.config.update(config)
     app.url_map.strict_slashes = False
 
-    # Model
-    from rmatics.model.base import db
     db.init_app(app)
-
-    # MongoDB
-    from rmatics.model.base import mongo
     mongo.init_app(app)
-
-    # Redis
-    from rmatics.model.base import redis
     redis.init_app(app)
 
     # Centrifugo
@@ -47,26 +58,9 @@ def create_app(config=None):
     cent_api_key = app.config.get('CENTRIFUGO_API_KEY')
     centrifugo_client.init_app(cent_url, cent_api_key)
 
-    # View
-    from rmatics.view import (
-        handle_api_exception,
-        load_user,
-    )
-    from werkzeug.exceptions import HTTPException
     app.register_error_handler(HTTPException, handle_api_exception)
     app.before_request(load_user)
 
-    from rmatics.view.user.auth import auth
-    from rmatics.view.bootstrap import bootstrap
-    from rmatics.view.course import course_blueprint
-    from rmatics.view.user.group import group
-    from rmatics.view.user.group_invite import group_invite
-    from rmatics.view.user.notification import notification
-    from rmatics.view.problem.route import problem_blueprint
-    from rmatics.view.protocol import protocol
-    from rmatics.view.course.statement import statement_blueprint
-    from rmatics.view.problem.submit import submit
-    from rmatics.view.user.user import user
     app.register_blueprint(auth)
     app.register_blueprint(bootstrap)
     app.register_blueprint(course_blueprint)
@@ -79,8 +73,9 @@ def create_app(config=None):
     app.register_blueprint(submit)
     app.register_blueprint(user)
 
+    app.cli.add_command(cli.test)
+
     # Utils
-    from rmatics.utils import url_encoder
     url_encoder.init_app(app)
 
     return app
