b565e24a8b397f8e619901a8663571839f96d84c
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2019-01-24 18:22:07 +0300

    feat (NFRMTCS-37): Add sql migrations for comments

--- migrations.sql
+++ migrations.sql
@@ -1,3 +1,11 @@
 USE pynformatics;
 ALTER TABLE runs add COLUMN source_hash varchar(32);
 ALTER TABLE runs add COLUMN ejudge_url varchar(50);
+
+USE ejudge;
+ALTER TABLE mdl_run_comments ADD COLUMN py_run_id INTEGER;
+UPDATE mdl_run_comments AS cmt INNER JOIN (
+    SELECT id, ej_run_id, ej_contest_id FROM pynformatics.runs
+) AS run
+ON run.ej_contest_id = cmt.contest_id AND run.ej_run_id = cmt.run_id
+SET cmt.py_run_id =run.id;
