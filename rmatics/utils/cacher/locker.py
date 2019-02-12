e8495127c7d74dfeed5ead33e25277bfb5765e73
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2019-02-12 18:40:49 +0300

    fix (NFRMTCS-65): Fixed RedisLocker, also added tests

--- rmatics/utils/cacher/locker.py
+++ rmatics/utils/cacher/locker.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
+import time
 
 from redlock import Redlock
 
 
 class ILocker(ABC):
     @abstractmethod
-    def lock(self, key, time=4000):
+    def lock(self, key, timeout=4000):
         pass
 
     @abstractmethod
@@ -23,11 +24,23 @@ class FakeLocker(ILocker):
 
 class RedisLocker(ILocker):
     def __init__(self, redis_con):
-        self.dlm = Redlock([redis_con, ])
+        self.dlm = Redlock([redis_con, ], retry_count=1)
         self._locks = {}
 
-    def lock(self, key, time=4000):
-        self._locks[key] = self.dlm.lock(key, time)
+    def lock(self, key, timeout=4000):
+        """ Trying to acquire lock
+            If not success then sleep (timeout // 10) milliseconds
+            and try again
+        """
+        sleep_time = timeout / (10 * 1000)
+        while True:
+            lock = self.dlm.lock(key, timeout)
+            if not lock:
+                time.sleep(sleep_time)
+                continue
+            break
+
+        self._locks[key] = lock
 
     def unlock(self, key):
         lock = self._locks[key]
