45fe853b09f775d3f8fad19cd80bdc2ff9cbb8f1
n.pakhtusov <n.pakhtusov@tinkoff.ru>
2019-02-13 19:17:09 +0300

    fix (NFRMTCS-65): Add logging for cron job

--- scripts/remove_cahce_meta/remove_cache_meta.py
+++ scripts/remove_cahce_meta/remove_cache_meta.py
@@ -7,13 +7,16 @@ from rmatics.model import CacheMeta
 # Cron every hour
 # 0 * * * * echo "прошёл один час"
 def main():
+    current_time = datetime.datetime.utcnow()
+    print(f'Job running at {current_time}')
     app = create_app()
     with app.app_context():
         db.session.query(CacheMeta). \
-            filter(CacheMeta.when_expire < datetime.datetime.utcnow()) \
+            filter(CacheMeta.when_expire < current_time) \
             .delete()
 
         db.session.commit()
+    print('Success')
 
 
 if __name__ == '__main__':
